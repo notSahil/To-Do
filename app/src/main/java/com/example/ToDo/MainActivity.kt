f38c45b067aeb4fd9cd21491eea6f5ffa0661c4e
SAHIL <sahilkumar848181@gmail.com>
2021-01-06 22:19:49 +0530

    nav

--- app/src/main/java/com/example/ToDo/MainActivity.kt
+++ app/src/main/java/com/example/ToDo/MainActivity.kt
@@ -13,6 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val actionbar =supportActionBar
+
+
+        actionbar!!.title = "Main"
+
+
 
 
     }
