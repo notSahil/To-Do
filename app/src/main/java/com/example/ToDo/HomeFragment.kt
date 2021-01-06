600174adbcfc3df064ef67bd06511f93d5f19f19
SAHIL <sahilkumar848181@gmail.com>
2021-01-07 02:20:47 +0530

    Toolbar

--- app/src/main/java/com/example/ToDo/HomeFragment.kt
+++ app/src/main/java/com/example/ToDo/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
 class HomeFragment : Fragment() {
@@ -13,6 +15,8 @@ class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+//toolbarCu.inflateMenu(R.menu.menu_toolbar)
+
     }
 
     override fun onCreateView(
@@ -20,8 +24,16 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val view  = inflater.inflate(R.layout.fragment_home, container, false)
+        view.floatingActionButtonn.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_noteTaking)
+
+        }
+
+        return view
     }
+
 
 
 }
--- app/src/main/java/com/example/ToDo/MainActivity.kt
+++ app/src/main/java/com/example/ToDo/MainActivity.kt
@@ -3,23 +3,38 @@ package com.example.ToDo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 //import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
+   private lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val actionbar =supportActionBar
+        //val actionbar =supportActionBar
+// all the main part here, for toolbar
+
+       val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.findNavController() // to set tool bar
+
+       // tool_barMain.title = "hhh"//
+        setSupportActionBar(tool_barMain)
+      //  setupActionBarWithNavController(navController)  // to set action bar title as LABEL
 
 
-        actionbar!!.title = "Main"
+       // actionbar!!.title = "Main"
+       // setSupportActionBar(tool_barMain)
 
+    }
 
-
-
+    override fun onSupportNavigateUp(): Boolean {
+       // return  navController.navigateUp()|| super.onSupportNavigateUp()   // to go back fragment with <- toolbar view
+   return super.onSupportNavigateUp()
     }
 }
