866f061d15f2c7f93cd3288df054780020927ee8
SAHIL <sahilkumar848181@gmail.com>
2021-01-09 17:52:41 +0530

    Fixed......................

--- app/src/main/java/com/example/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/add/AddFragment.kt
@@ -1,5 +1,3 @@
-package com.example.todoapp.fragments.add
-
 
 import android.os.Bundle
 import android.view.*
@@ -22,8 +20,8 @@ class AddFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentAddBinding.inflate(layoutInflater, container, false)
@@ -57,10 +55,10 @@ class AddFragment : Fragment() {
         if(validation){
             // Insert Data to Database
             val newData = ToDoData(
-                0,
-                mTitle,
-                mSharedViewModel.parsePriority(mPriority),
-                mDescription
+                    0,
+                    mTitle,
+                    mSharedViewModel.parsePriority(mPriority),
+                    mDescription
             )
             mToDoViewModel.insertData(newData)
             Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_SHORT).show()
@@ -76,75 +74,3 @@ class AddFragment : Fragment() {
         _binding = null
     }
 }
-
-
-
-
-
-
-//import com.example.ToDo.databinding.FragmentAddBinding
-//import com.example.ToDo.fragments.SharedViewModel
-//
-//class AddFragment : Fragment() {
-//
-//    private val mToDoViewModel: ToDoViewModel by viewModels()
-//    private val mSharedViewModel: SharedViewModel by viewModels()
-//
-//    private var _binding: FragmentAddBinding? = null
-//    private val binding get() = _binding!!
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        // Inflate the layout for this fragment
-//        _binding = FragmentAddBinding.inflate(layoutInflater, container, false)
-//
-//        // Set Menu
-//        setHasOptionsMenu(true)
-//
-//        // Spinner Item Selected Listener
-//       binding.prioritiesSpinner.onItemSelectedListener = mSharedViewModel.listener
-//
-//        return binding.root
-//    }
-//
-//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-//        inflater.inflate(R.menu.add_fragment_menu, menu)
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        if(item.itemId == R.id.menu_add){
-//          //  insertDataToDb()
-//        }
-//        return super.onOptionsItemSelected(item)
-//    }
-//
-//    private fun insertDataToDb() {
-//        val mTitle = binding.titleEt.text.toString()
-//        val mPriority = binding.prioritiesSpinner.selectedItem.toString()
-//        val mDescription = binding.descriptionEt.text.toString()
-//
-//        val validation = mSharedViewModel.verifyDataFromUser(mTitle, mDescription)
-//        if(validation){
-//            // Insert Data to Database
-//            val newData = ToDoData(
-//                0,
-//                mTitle,
-//                mSharedViewModel.parsePriority(mPriority),
-//                mDescription
-//            )
-//            mToDoViewModel.insertData(newData)
-//            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_SHORT).show()
-//            // Navigate Back
-//            findNavController().navigate(R.id.action_addFragment_to_listFragment)
-//        }else{
-//            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_SHORT).show()
-//        }
-//    }
-//
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        _binding = null
-//    }
-//}
